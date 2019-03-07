cf439acc61527eca209f806bd52467c03dc2efe3
Brian Pye <5097038+beepye@users.noreply.github.com>
2019-03-07 10:00:29 -0500

    Updating comments with thoughts

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@
 
 /*  
 On clicking the Add Meal button:
-	1. Grab the value of each input and assign to a variable
+	1. Grab the value of each input and assign to a variable - DONE!
 	2. Use those input values to create a new meal object
-		a. Does the item name already exist?
-		b. If yes do not validate, prompt user to change name
-		c. If no then create the meal obj
-	3. Populate the mealList array with the new meal object
+		a. Do both inputs have a value? - DONE!
+			i. Yes: proceed with adding the meal
+			ii. No: alert user and bail
+		b. Does the item name already exist?
+			i. Yes: do not validate, prompt user to change name
+			ii. No: then create the meal obj
+	3. Populate the mealList array with the new meal object - DONE!
 */
 
 var mealList = [];
