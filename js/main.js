d729b9a07cf74ef9930a9ffe0d1caf38744189b1
Brian Pye <5097038+beepye@users.noreply.github.com>
2019-03-06 21:31:59 -0500

    Added skeleton.css | Added beginning scripts (incomplete) | Added html form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 'use strict';
 
-var mealList = [];
+/*  
+	On clicking the Add Meal button:
+		1. Grab the value of each input and assign to a variable
+		2. Use those input values to create a new meal object
+			a. Does the item name already exist?
+			b. If yes do not validate, prompt user to change name
+			c. If no then create the meal obj
+		3. Populate the mealList array with the new meal object
+*/
 
+var mealList = [],
+	meal_name = document.querySelectorAll('[name="meal_name"]'),
+	meal_typel = document.querySelectorAll('[name="meal_type"]');
+
+// Meal object proto
 const Meal = function(meal_name, meal_type) {
 	this.meal_name = meal_name,
 	this.meal_type = meal_type;
