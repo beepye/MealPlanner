330f21357354a09ce7d311dcd2b658f6a617502c
Brian Pye <5097038+beepye@users.noreply.github.com>
2019-03-11 16:27:54 -0400

    Added Babel for transpiling

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,19 @@ module.exports = {
 	output: {
 		filename: 'main.js',
 		publicPath: 'dist'
-	}
+	},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
