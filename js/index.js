7dc56ac093a004c87f03170d8f6c9ffa8ad67d72
Brian Pye <beepye@gmail.com>
2019-03-28 09:48:59 -0400

    Working on xml request code

--- js/index.js
+++ js/index.js
@@ -1,38 +1,33 @@
 (function() {
 	'use strict';
 
+	const GENERATE_BTN = document.getElementById('GenerateMealList');
+
 	var requestURL = 'http://www.thebrianpye.com/Recipes/meals.json',
 			request = new XMLHttpRequest();
 
 	request.responseType = 'json';
-	request.addEventListener('load', doTheDamnThing);
+	// request.addEventListener('load', doTheDamnThing);
 	request.open('GET', requestURL);
 	request.send();
 
-	// request.onload = function() {
-	//   const Meals = request.response;
-	//   console.log(Meals[0]);
+	request.onload = function() {
+	  const MEALS = request.response;
+	  console.log(MEALS);
+	}
+
+	// GENERATE_BTN.addEventListener('click', function (e) {
+	// 	alert('Nothing is functioning yet');
+	// })
+
+	// function doTheDamnThing() {
+	//   getBriansMeals();
 	// }
 
-	function doTheDamnThing() {
-	  logOurMealLists();
-	}
-
-	var logOurMealLists = function() {
-	  getBriansMeals();
-	  getRonsMeals();
-	}
-
-	var getBriansMeals = function() {
-	  const Meals = request.response;
-		var myMeals = Meals.filter(function(whom){ return whom.owner === 'Brian'}).map(function(whom){ return whom.name; });
-		console.log(myMeals);
-	}
-
-	var getRonsMeals = function() {
-	  const Meals = request.response;
-		var myMeals = Meals.filter(function(whom){ return whom.owner === 'Ronnie'}).map(function(whom){ return whom.name; });
-		console.log(myMeals);
-	}
+	// var getBriansMeals = function() {
+	//   const Meals = request.response;
+	// 	var myMeals = Meals.filter(function(whom){ return whom.owner === 'Brian'}).map(function(whom){ return whom.name; });
+	// 	console.log(myMeals);
+	// }
 
 }());
