f0d84335b684ebfe8e5244bf6f88aaa4a574ae60
Brian Pye <beepye@gmail.com>
2019-03-31 14:21:10 -0400

    Working on XHR script: currently trying to return the JSON into an array variable to manipulate | Updated some node mods

--- js/index.js
+++ js/index.js
@@ -1,31 +1,40 @@
 (function() {
 	'use strict';
 
-	const GENERATE_BTN = document.getElementById('GenerateMealList');
+	const GENERATE_BTN = document.getElementById('GenerateMealList'),
+		 		URL = 'http://www.thebrianpye.com/Recipes/meals.json';
 
-	var requestURL = 'http://www.thebrianpye.com/Recipes/meals.json',
-			request = new XMLHttpRequest();
+	function getMeals(callback) {
+		// Set up initial request
+		var	xhr = new XMLHttpRequest();
+		xhr.responseType = 'json';
+		xhr.open('GET', URL);
+		// Once the response status is 200 return the response
+		xhr.onload = function() { 
+			callback(xhr.response);
+		};
 
-	request.responseType = 'json';
-	// request.addEventListener('load', doTheDamnThing);
-	request.open('GET', requestURL);
-	request.send();
-
-	request.onload = function() {
-	  const MEALS = request.response;
-	  console.log(MEALS);
+		xhr.send();
 	}
 
-	// GENERATE_BTN.addEventListener('click', function (e) {
-	// 	alert('Nothing is functioning yet');
-	// })
+	var list = []
+
+	getMeals(function(response) {
+		// Cycle through each obj in the arr
+		response.forEach(function(item) {
+			console.log(item);
+			return list.push(item);
+		});
+
+		console.log(list.length);
+	});
 
 	// function doTheDamnThing() {
 	//   getBriansMeals();
 	// }
 
 	// var getBriansMeals = function() {
-	//   const Meals = request.response;
+	// 	const Meals = request.response;
 	// 	var myMeals = Meals.filter(function(whom){ return whom.owner === 'Brian'}).map(function(whom){ return whom.name; });
 	// 	console.log(myMeals);
 	// }
