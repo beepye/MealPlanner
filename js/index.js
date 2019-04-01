637ef81ed09e0d95e18531f8f38f5e409cd48988
Brian Pye <beepye@gmail.com>
2019-04-01 16:02:13 -0400

    Adding function to build html for new menu

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@
 				mealList.push(item);
 			});
 
-			selectRandomMeals(days, mealList);
+			selectRandomMeals(days, mealList, createMealListHTML);
 		});
 	});
 
@@ -27,17 +27,34 @@
 		xhr.open('GET', URL);
 		// Pass the response in a callback function
 		xhr.onload = function() { callback(xhr.response); };
-
 		xhr.send();
 	}
-	
-	var selectRandomMeals = function selectRandomMeals(days, arr) {
+
+	var selectRandomMeals = function selectRandomMeals(days, arr, callback) {
 		// Reorder array in random sequence
 		let randomlyOrderedArray = arr.sort(function() { return .5 - Math.random(); });
 		// Take re-ordered array and only keep as many as needed
 		let newArray = randomlyOrderedArray.slice(0, days);
-		console.log(newArray);
+		callback(newArray);
 	}
+
+	var createMealListHTML = function createMealListHTML(arr) {
+		let container = document.createElement('section'),
+				main = document.querySelector('main'),
+				list = document.createElement('ul'),
+				item = document.createElement('li'),
+				itemLink = document.createElement('a'),
+				listItem = item.append(itemLink);
+		// Build list for meals
+		main.append(container);
+		container.append(list);
+		// console.log(arr);
+
+		arr.forEach(function(item) {
+			console.log(item.name);
+		});
+	} 
+	// createMealListHTML();
 
 	// 	var myMeals = Meals.filter(function(whom){ return whom.owner === 'Brian'}).map(function(whom){ return whom.name; });
 
